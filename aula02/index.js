c2bbf2330259bf882c6288ff0369ac25b174d1d7
Giulliano Antonini <giullianoantonini@gmail.com>
2024-08-01 22:03:52 -0300

    feat: adiciona aula 02 sobre npm

---
+++ aula02/index.js
@@ -0,0 +1,6 @@
+const date = new Date();
+const day = date.getDate();
+const month = date.getMonth() + 1;
+const year = date.getFullYear();
+
+console.log(`Hoje é o dia ${day}/${month}/${year}!!!`);
