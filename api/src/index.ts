5a77d69cf4a44580412754158198feeef501c660
Giulliano Antonini <giullianoantonini@gmail.com>
2024-08-12 22:02:57 -0300

    aula 05

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,46 @@
-console.log ("Em breve uma API")
+import express, { Request, Response } from "express";
+
+const app = express();
+app.use(express.static("public"));
+const porta = 3030;
+const num1: number = 1;
+const num2: number = 2;
+const num3: number = 3;
+const soma: number = num1 + num2 + num3;
+const somaString: string = soma.toString();
+
+app.get("/", (req: Request, res: Response): void => {
+  res.send("API em NodeJS com TS");
+});
+
+app.get("/ping", (req: Request, res: Response): void => {
+  res.send("Pong 🏓");
+});
+
+app.get("/soma", (req: Request, res: Response): void => {
+  // http://localhost:3030?num1=1&num2=1&num3=5
+
+  let { num1, num2, num3 } = req.query as {
+    num1: string;
+    num2: string;
+    num3: string;
+  };
+  const soma = parseInt(num1, 10) + parseInt(num2, 10) + parseInt(num3, 10);
+
+  res.send(`${soma}`);
+});
+
+app.get("/departamentos/:id", (req: Request, res: Response): void => {
+  console.log(req.params);
+
+  res.send("Pong 🏓");
+});
+
+app.get("/clientes", (req: Request, res: Response): void => {
+  const html: string =
+    '<link href="estilo.css" rel="stylesheet"><h1>Clientes</h1>';
+});
+
+app.listen(porta, () =>
+  console.log(`Servidor escutando na porta http://localhost:${porta}`)
+);
