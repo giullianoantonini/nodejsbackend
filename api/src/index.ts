4865a26b95da1c6a86c07645c7b877e699194fee
Giulliano Antonini <giullianoantonini@gmail.com>
2024-08-14 20:33:42 -0300

    feat: postman

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from "express";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static("public"));
 const porta = 3030;
 const num1: number = 1;
@@ -39,8 +43,22 @@ app.get("/departamentos/:id", (req: Request, res: Response): void => {
 app.get("/clientes", (req: Request, res: Response): void => {
   const html: string =
     '<link href="/css/estilo.css" rel="stylesheet"><h1>Clientes</h1>';
-  res.send(html)
-  });
+  res.send(html);
+});
+
+app.get("/funcionarios", (req: Request, res: Response): void => {
+  const funcionario = {
+    nome: "Giulliano",
+    salario: "5000.00",
+  };
+  res.json(funcionario);
+});
+
+app.post("/departamentos", (req: Request, res: Response): void => {
+  console.log(req.body);
+
+  res.send("post departamentos");
+});
 
 app.listen(porta, () =>
   console.log(`Servidor escutando na porta http://localhost:${porta}`)
