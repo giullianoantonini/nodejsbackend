1cad17f9b9287105da6ad4611dd4fec1be0e177a
Giulliano Antonini <giullianoantonini@gmail.com>
2024-08-19 21:06:58 -0300

    .

--- api/src/controllers/defaultControllers.ts
+++ api/src/controllers/defaultControllers.ts
@@ -2,7 +2,7 @@
 import { Request, Response } from "express";
 
 const rootResponse = (req: Request, res: Response): void => {
-  res.send("API em NodeJS com TS em MVC");
+  res.send("API em NodeJS com TS em MVC - hospedada na Vercel");
 }
 
 export default rootResponse;
