206e909c5f8ccb924e000c487cf0ccb676632149
Giulliano Antonini <giullianoantonini@gmail.com>
2024-08-19 21:08:09 -0300

    .

--- api/dist/controllers/defaultControllers.js
+++ api/dist/controllers/defaultControllers.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const rootResponse = (req, res) => {
-    res.send("API em NodeJS com TS em MVC");
+    res.send("API em NodeJS com TS em MVC - hospedada na Vercel");
 };
 exports.default = rootResponse;
 //# sourceMappingURL=defaultControllers.js.map
