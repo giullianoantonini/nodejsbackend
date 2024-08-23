278fce40511e0f34c27902306768bd2ea71f156e
Giulliano Antonini <giullianoantonini@gmail.com>
2024-08-23 15:38:56 -0300

    corrige cors

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,9 +2,12 @@ import express, { Request, Response } from "express";
 import defaultRoutes from "./routes/defaultRoutes";
 import departamentosRoutes from "./routes/departamentosRoutes";
 import funcionariosRoutes from "./routes/funcionariosRoutes";
+import cors from "cors";
 
 const porta = 3030;
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(defaultRoutes);
