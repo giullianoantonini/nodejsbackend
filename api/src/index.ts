1ac4e3b3d12c222922156d556bac121c8d0797a2
Giulliano Antonini <giullianoantonini@gmail.com>
2024-08-23 15:20:43 -0300

    finaliza aula 10

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,13 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import defaultRoutes from "./routes/defaultRoutes";
 import departamentosRoutes from "./routes/departamentosRoutes";
 import funcionariosRoutes from "./routes/funcionariosRoutes";
+import cors from "cors";
 
 const porta = 3030;
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(defaultRoutes);
